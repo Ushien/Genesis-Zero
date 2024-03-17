c6632a649fb4aa5fa302884d271e9399aebfebef
Ushien <antoinevanaelst@hotmail.com>
2024-03-17 10:35:44 +0100

    Squelette du système de tours

--- Assets/_Scripts/Managers/GameManager.cs
+++ Assets/_Scripts/Managers/GameManager.cs
@@ -4,7 +4,15 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    
     public static GameManager Instance;
+    
+    public enum BattleState {OUTSIDE, START, PLAYERTURN, ENEMYTURN, END, WON, LOST}
+    public enum TurnState {OUTSIDE, START, MIDDLE, END}
+
+    public int nTurn = 0;
+    public BattleState battleState;
+    public TurnState turnState;
 
     void Awake(){
         Instance = this;
@@ -16,11 +24,30 @@ public class GameManager : MonoBehaviour
         GridManager.Instance.GenerateGrid();
         UnitManager.Instance.SpawnEnemy(1, 1);
         UnitManager.Instance.SpawnEnemy(1, 3);
+        StartBattle();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void StartBattle(){
+        battleState = BattleState.START;
+
+        //Start the first turn
+        NextTurn();
+
+    }
+
+    private void StartTurn(){
+        turnState = TurnState.START;
+    }
+
+    [ContextMenu("Tour suivant")]
+    public void NextTurn(){
+        nTurn ++;
+        StartTurn();
     }
 }
