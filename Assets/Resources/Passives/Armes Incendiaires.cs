315d7cc8a149394221cf8710dac025f6d038deec
Ushien <antoinevanaelst@hotmail.com>
2024-04-08 00:16:13 +0200

    Transfert commit

---
+++ Assets/Resources/Passives/Armes Incendiaires.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : Passive
+{
+    // Les attaques de base du personnage font des dégats supplémentaires et appliquent l'élément feu.
+
+    // Lorsque le passif est setup, l'active
+
+    // Lorsque le passif disparaît, le désactive
+}
---
+++ Assets/_Scripts/Passives/Passive.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Passive : MonoBehaviour
+{
+    public String fight_description;
+}
