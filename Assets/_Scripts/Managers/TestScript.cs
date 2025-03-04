2f712522c444f5f2fc92b94b65f566e51537f24f
Ushien <antoinevanaelst@hotmail.com>
2025-03-04 23:29:42 +0900

    Les instructions scriptées se vident automatiquement

--- Assets/_Scripts/Managers/TestScript.cs
+++ Assets/_Scripts/Managers/TestScript.cs
@@ -16,7 +16,7 @@ public class TestScript : MonoBehaviour
     public ScriptableComposition ally_composition;
     public List<ScriptableObject> rewardsToSpawn;
     public static TestScript Instance;
-    private string scriptedInstructions = "A00-0-E02/A10-0-E12/A20-0-E22";
+    private List<string> scriptedInstructions = new List<string>(){"A00-0-E02/A10-0-E12/A20-0-E22"};
 
     public void Awake(){
         Instance = this;
@@ -33,41 +33,48 @@ public class TestScript : MonoBehaviour
 
     public List<Instruction> GetScriptedInstructions(){
 
-        string[] stringInstructions = scriptedInstructions.Split("/");
-        List<Instruction> instructions = new List<Instruction>();
-        foreach (string stringInstruction in stringInstructions)
-        {
-            string[] subInstructions = stringInstruction.Split("-");
-            Instruction newInstruction = Instantiate(AIManager.Instance.emptyInstruction);
-            Team team = Team.Enemy;
-            if(subInstructions[0][0] == 'E'){
-                team = Team.Enemy;
-            }
-            else if(subInstructions[0][0] == 'A'){
-                team = Team.Ally;
-            }
-            Assert.AreEqual(team, BattleManager.Instance.ConvertTeamTurn(BattleManager.Instance.teamTurn));
-            BaseUnit sourceUnit = GridManager.Instance.GetTileAtPosition(team, new Vector2(Int32.Parse(subInstructions[0][1].ToString()), Int32.Parse(subInstructions[0][2].ToString()))).GetUnit();
-            
-            BaseSpell spellToCast = sourceUnit.GetSpells(includingAttack : true)[Int32.Parse(subInstructions[1])];
-            
-            if(subInstructions[2][0] == 'E'){
-                team = Team.Enemy;
-            }
-            else if(subInstructions[2][0] == 'A'){
-                team = Team.Ally;
-            }
-            Tile targetTile = GridManager.Instance.GetTileAtPosition(team, new Vector2(Int32.Parse(subInstructions[2][1].ToString()), Int32.Parse(subInstructions[2][2].ToString())));
-            newInstruction.Setup(sourceUnit,spellToCast, targetTile);
-            instructions.Add(newInstruction);
-            Debug.Log(newInstruction.GetSummary());
-        }
+        if(AreThereScriptedInstructions()){
 
-        return instructions;
+            string[] stringInstructions = scriptedInstructions[0].Split("/");
+            scriptedInstructions.RemoveAt(0);
+            List<Instruction> instructions = new List<Instruction>();
+            foreach (string stringInstruction in stringInstructions)
+            {
+                string[] subInstructions = stringInstruction.Split("-");
+                Instruction newInstruction = Instantiate(AIManager.Instance.emptyInstruction);
+                Team team = Team.Enemy;
+                if(subInstructions[0][0] == 'E'){
+                    team = Team.Enemy;
+                }
+                else if(subInstructions[0][0] == 'A'){
+                    team = Team.Ally;
+                }
+                Assert.AreEqual(team, BattleManager.Instance.ConvertTeamTurn(BattleManager.Instance.teamTurn));
+                BaseUnit sourceUnit = GridManager.Instance.GetTileAtPosition(team, new Vector2(Int32.Parse(subInstructions[0][1].ToString()), Int32.Parse(subInstructions[0][2].ToString()))).GetUnit();
+                
+                BaseSpell spellToCast = sourceUnit.GetSpells(includingAttack : true)[Int32.Parse(subInstructions[1])];
+                
+                if(subInstructions[2][0] == 'E'){
+                    team = Team.Enemy;
+                }
+                else if(subInstructions[2][0] == 'A'){
+                    team = Team.Ally;
+                }
+                Tile targetTile = GridManager.Instance.GetTileAtPosition(team, new Vector2(Int32.Parse(subInstructions[2][1].ToString()), Int32.Parse(subInstructions[2][2].ToString())));
+                newInstruction.Setup(sourceUnit,spellToCast, targetTile);
+                instructions.Add(newInstruction);
+            }
+
+            return instructions;
+        }
+        else{
+            return null;
+        }
+        
     }
 
     public bool AreThereScriptedInstructions(){
-        return true;
+        return scriptedInstructions.Count > 0;
     }
 
     public void TestAssertions(){
