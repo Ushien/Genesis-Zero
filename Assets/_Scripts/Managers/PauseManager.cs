d050c6c0d761af8ec52cc66332c75341f37bc885
Ushien <antoinevanaelst@hotmail.com>
2025-07-30 00:33:53 +0200

    Simplification du code
    On chasse les vilaines updates

--- Assets/_Scripts/Managers/PauseManager.cs
+++ Assets/_Scripts/Managers/PauseManager.cs
@@ -9,15 +9,10 @@ public class PauseManager : MonoBehaviour
     public static PauseManager Instance;
     [SerializeField]
     GameObject pauseMenu;
-    private bool stateChanged = false;
     public bool isPaused { get; private set; }
     private PlayerInput playerInput;
     private string previousActionMap;
 
-    void Update()
-    {
-        stateChanged = false;
-    }
     void Awake()
     {
         Instance = this;
@@ -26,36 +21,22 @@ public class PauseManager : MonoBehaviour
 
         playerInput.actions["Pause"].performed += PauseGame;
         playerInput.actions["Unpause"].performed += UnpauseGame;
-        /*
-        PlayerInputActions playerInputActions = new PlayerInputActions();
-        playerInputActions.Enable();
-        playerInputActions.Player.Pause.started += PauseGame;
-        playerInputActions.Pause.Unpause.started += UnpauseGame;
-        */
     }
 
     public void PauseGame(InputAction.CallbackContext context)
     {
-        if (!stateChanged && !isPaused)
-        {
-            pauseMenu.SetActive(true);
-            isPaused = true;
-            Time.timeScale = 0.5f;
-            stateChanged = true;
-            previousActionMap = playerInput.currentActionMap.id.ToString();
-            playerInput.SwitchCurrentActionMap("Pause");
-        }
+        pauseMenu.SetActive(true);
+        isPaused = true;
+        Time.timeScale = 0.5f;
+        previousActionMap = playerInput.currentActionMap.id.ToString();
+        playerInput.SwitchCurrentActionMap("Pause");
     }
 
     public void UnpauseGame(InputAction.CallbackContext context)
     {
-        if (!stateChanged && isPaused)
-        {
-            pauseMenu.SetActive(false);
-            isPaused = false;
-            Time.timeScale = 1f;
-            stateChanged = true;
-            playerInput.SwitchCurrentActionMap(previousActionMap);
-        }
+        pauseMenu.SetActive(false);
+        isPaused = false;
+        Time.timeScale = 1f;
+        playerInput.SwitchCurrentActionMap(previousActionMap);
     }
 }
